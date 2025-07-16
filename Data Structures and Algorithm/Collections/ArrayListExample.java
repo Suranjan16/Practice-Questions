fd54049381fe4b55044fe6ed6e9d68ac0c72908c
Suranjan16 <suranjanmaity851@gmail.com>
2025-07-17 02:30:12 +0530

    added some programs

---
+++ Data Structures and Algorithm/Collections/ArrayListExample.java
@@ -0,0 +1,16 @@
+// package Data Structures and Algorithm.Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(10);
+        list.add("code");
+        list.add(true);
+
+        System.out.println("Size: " + list.size());
+        System.out.println("Elements: " + list);
+    }
+}
+
--- Data Structures and Algorithm/Strings/EqualsVsEquals.java
+++ Data Structures and Algorithm/Strings/EqualsVsEquals.java
@@ -1,4 +1,5 @@
 public class EqualsVsEquals {
+    @SuppressWarnings("StringEquality")
     public static void main(String[] args) {
         String a = "Suranjan";
         String b = "Suranjan";
